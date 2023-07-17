d3cb319c9687ec8606c8177e56ea4cca95e18fb9
kjm232311 <kjm232311@gmail.com>
2023-07-17 20:52:47 +0900

    third commit

--- polls/admin.py
+++ polls/admin.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
 from .models import Question, Choice
 
-# Register your models here.
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
 
-admin.site.register(Question)
-admin.site.register(Choice)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('question_text', 'pub_date')
+
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
--- polls/views.py
+++ polls/views.py
@@ -11,6 +11,9 @@ class IndexView(generic.ListView):
     template_name = "polls/index.html"
     context_object_name = "latest_question_list"
 
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
+
 class DetailView(generic.DetailView):
     model = Question
     template_name = "polls/detail.html"
@@ -37,4 +40,7 @@ def vote(request, question_id):
         
         return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
 
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 # Create your views here.
